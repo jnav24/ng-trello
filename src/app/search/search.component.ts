78ec497b445e38579591af30883e17769edb0aef
Justin Navarro <info@justinnavarro.net>
2019-07-26 18:10:23 -0400

    get the trello state from the app store

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AppState} from '../store/app.state';
+import {Store} from '@ngrx/store';
+import {Observable, Subscription} from 'rxjs';
+import {TrelloModel} from '../store/models/trello.model';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+    trello$: TrelloModel;
+    trelloSubscription: Subscription;
 
-  constructor() { }
+    constructor(
+        private store: Store<AppState>,
+    ) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.trelloSubscription = this.store.select(store => store.trello).subscribe((trelloState) => this.trello$ = trelloState);
+    }
 
+    ngOnDestroy() {
+        this.trelloSubscription.unsubscribe();
+    }
 }
