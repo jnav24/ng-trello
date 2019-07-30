b45729ec1be60557306e6d85acc49203cac4ebba
Justin Navarro <info@justinnavarro.net>
2019-07-29 20:22:04 -0400

    add the method to get all cards

--- src/app/services/trello.service.ts
+++ src/app/services/trello.service.ts
@@ -2,23 +2,34 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {BoardsModel} from '../store/models/board.model';
+import {CardModel} from '../store/models/card.model';
 
 @Injectable()
 export class TrelloService {
-    private readonly key = environment.trello_key;
-    private readonly token = environment.trello_token;
     private readonly url = environment.trello_url;
+    private readonly params = {
+        key: environment.trello_key,
+        token: environment.trello_token,
+    };
 
     constructor(
         private http: HttpClient,
     ) {}
 
     getBoards() {
+        return this.http.get<BoardsModel[]>(`${this.url}members/me/boards`, {
+            params: {
+                ...this.params,
+                fields: 'name,url'
+            },
+        });
+    }
+
+    getAllCards(boardId: string) {
         const params = {
-            key: this.key,
-            fields: 'name,url',
-            token: this.token,
+            ...this.params,
+            fields: 'name,idList,url,idLabels,desc',
         };
-        return this.http.get<BoardsModel[]>(`${this.url}members/me/boards`, { params });
+        return this.http.get<CardModel[]>(`${this.url}boards/${boardId}/cards`, { params });
     }
 }
